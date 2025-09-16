aa8535d6ec21efb154d5c0dc50ab5c8ed4f4a22e
南楠 <nannan@192.168.2.37>
2025-09-16 20:52:55 +0800

    chore: push

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,15 +2,7 @@
   <footer
     class="mt-16 flex-none mb-12 border-t pt-8 border-[color:var(--color-base-300)]"
   >
-    <div class="flex flex-col items-center gap-6 lg:flex-row justify-between">
-      <!-- 左侧导航 -->
-      <ul class="flex items-center gap-2 lg:gap-6 lg:items-center text-sm">
-        <li>About</li>
-        <li>Projects</li>
-        <li>Speaking</li>
-        <li>Uses</li>
-      </ul>
-
+    <div class="flex flex-col items-center gap-6 lg:flex-row justify-end">
       <!-- 右侧版权信息 -->
       <p class="text-gray-500 text-sm text-center lg:text-right">
         <span>© </span>
