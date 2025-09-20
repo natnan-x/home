ae5ff338d0a995e4f280ed4da85db1fd1428574f
南楠 <nannan@192.168.2.61>
2025-09-20 17:27:52 +0800

    chore; push

---
+++ src/views/demo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="p-8">
+      <button class="btn btn-primary">Primary Button</button>
+      <button class="btn btn-secondary">Secondary Button</button>
+    </div>
+  </div>
+</template>
