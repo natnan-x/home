fd0e0d0dca0fc47482714d2c049326ea8b631db2
南楠 <nannan@192.168.2.61>
2025-09-23 03:49:09 +0800

    ok:about

--- src/data/en/about.js
+++ src/data/en/about.js
@@ -1,9 +1,10 @@
 export default {
   title: "I’m NatNan. I live in ChengDu City, where I design the future.",
   content: [
-    "I’ve loved making things for as long as I can remember, and wrote my first program when I was 6 years old, just two weeks after my mom brought home the brand new Macintosh LC 550 that I taught myself to type on.",
-    "The only thing I loved more than computers as a kid was space. When I was 8, I climbed the 40-foot oak tree at the back of our yard while wearing my older sister’s motorcycle helmet, counted down from three, and jumped — hoping the tree was tall enough that with just a bit of momentum I’d be able to get to orbit.",
-    "I spent the next few summers indoors working on a rocket design, while I recovered from the multiple surgeries it took to fix my badly broken legs. It took nine iterations, but when I was 15 I sent my dad’s Blackberry into orbit and was able to transmit a photo back down to our family computer from space.",
-    "Today, I’m the founder of Planetaria, where we’re working on civilian space suits and manned shuttle kits you can assemble at home so that the next generation of kids really can make it to orbit — from the comfort of their own backyards.",
+    `I’ve always been curious about the world and rarely listen to ‘what others say.’ Trial and error is my favorite teacher—though I still take grandma’s advice."`,
+    `As I grew up, I met many friends: colleagues, basketball buddies, and cycling companions. Every encounter adds a splash of color to life.`,
+    `I have many passions: Higashino Keigo’s novels, exercise, iced Americanos, and cute little animals. Each teaches me something. Higashino once said, 'There are two things you cannot stare at directly: the sun and the human heart.' Me? I choose to keep my heart towards the light. Exercise fuels me like I could go another five hundred years. My three cats and one dog taught me responsibility.`,
+    `Now, I’m a front-end developer. Coding feels like solving puzzles and stacking blocks—every line brings a little surprise. New technologies keep coming, whispering: don’t stand still. Embrace change, try boldly—that’s a game for the brave.`,
+    `Sincerity, trust, responsibility, positivity, and always facing the sun—these are my labels and my life compass.`,
   ],
 };
